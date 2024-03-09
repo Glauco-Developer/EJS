ffac13ff1c14b85688c003808da204aa5c378e6a
glaucorusso <glaucorusso@ebow.ie>
2024-03-09 20:28:06 -0300

    added bodyParser middleware

--- index.js
+++ index.js
@@ -1,29 +1,22 @@
 import express from "express";
+import bodyParser from "body-parser"
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-const __dirname = dirname(fileURLToPath(import.meta.url));
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
+
+// Middleware Function
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + "/public/index.html");
 });
 
-app.post('/register', (req, res) => {
-    res.sendStatus(201);
-});
-
-app.put('/user/glauco', (req, res) => {
-    res.sendStatus(200);
-});
-
-app.patch('/user/glauco', (req, res) => {
-    res.sendStatus(200);
-});
-
-app.delete('/user/glauco', (req, res) => {
-    res.sendStatus(200);
+app.post('/login', (req, res) => {
+    console.log(req.body);
+    res.sendFile(__dirname + "/public/dashboard.html");
 });
 
 app.listen(port, () => {
