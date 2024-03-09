6ac242fe33ca5a342949c1a90cc6176dda817345
glaucorusso <glaucorusso@ebow.ie>
2024-03-09 20:33:20 -0300

    middleware functions (morgan)

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
-import bodyParser from "body-parser"
+import bodyParser from "body-parser";
+import morgan from "morgan";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 
@@ -7,8 +8,9 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
 
-// Middleware Function
+// Middleware Functions
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("tiny"));
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + "/public/index.html");
