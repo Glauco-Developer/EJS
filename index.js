5c9c6b04a63bf4895cfb36ae3fac912a95c1681a
glaucorusso <glaucorusso@ebow.ie>
2024-03-13 15:29:45 -0300

    EJS syntax examples

--- index.js
+++ index.js
@@ -8,21 +8,13 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    const today = new Date();
-    const day = today.getDay();
-
-    let type = "a weekday"
-    let adv = "it's time to work hard"
-
-    if(day === 0 || day === 6) {
-        type = "the weekend"
-        adv = "it's time to have fun"
-    }
-
-    res.render("index.ejs", {
-        dayType: type,
-        advice: adv,
-    });
+    const data = {
+        title: "EJS Tags",
+        seconds: new Date().getSeconds(),
+        items: ["apple", "banana", "cherry"],
+        htmlContent: "<strong>Strong text</strong>"
+    };
+    res.render("index.ejs", data);
 });
 
 app.listen(port, () => {
